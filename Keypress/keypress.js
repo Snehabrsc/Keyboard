2d79a74e302b94155c49a376ee3ca0b0b868b35b
Snehasree <snehabrsc@gmail.com>
2025-02-06 15:00:50 +0000

    Keycode

---
+++ Keypress/keypress.js
@@ -0,0 +1,24 @@
+const insert=document.getElementById("insert")
+
+window.addEventListener('keydown',(e) =>{
+    console.log(e)
+    insert.innerHTML=`
+    <div class='code'>
+    <table>
+        <tr>
+            <th>key</th>
+            <th>keycode</th>
+            <th>code</th>
+        </tr>
+        <tr>
+            <td>${e.key===" "? "space" :e.key}</td>
+            <td>${e.keyCode}</td>
+            <td>${e.code }</td>
+        </tr>
+  
+    </table>
+    </div>
+    
+    
+    `
+})
